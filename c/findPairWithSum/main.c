78510451c38072016975d55a7fd4e416d159edba
zachlefevre <zachlefevre@gmail.com>
2017-09-06 12:33:19 -0500

    Implemented findPair on sets

--- c/findPairWithSum/main.c
+++ c/findPairWithSum/main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-int *findPair(int *arr, int size, int sum){
+int *findPairList(int *arr, int size, int sum){
   int i;
   int low = 0;
   int high = size - 1;
@@ -16,10 +16,27 @@ int *findPair(int *arr, int size, int sum){
   return NULL;
 }
 
+int *findPairSet(int *arr, int size, int sum){
+  int i;
+  int low = 0;
+  int high = size - 1;
+  static int pair[2];
+  while(low <= high){
+    if(arr[low] + arr[high] == sum){
+      pair[0] = arr[low];
+      pair[1] = arr[high];
+      return pair;
+    }
+    (arr[low] + arr[high] < sum)? low++: high--;
+  }
+  return NULL;
+}
+
 int main(void){
-  int a[] = {1,2,3,3,4,6,8,8,9};
+  int a[] = {1,2,3};
   int size = sizeof(a)/sizeof(a[0]);
-  int *pair = findPair(a, size, 16);
+  int *pair = findPairList(a, size, 16);
+  pair = findPairSet(a, size, 6);
   if(pair){
     printf("pair: %d, %d\n", pair[0], pair[1]);
   }else{
